d743da751cb3643ee6f4e0afb62f8a6002c36f90
sauravsurve <sau97.surve@gmail.com>
2018-11-15 11:31:56 +0530

    Updated comments

--- shortestPathAlgorithm.cpp
+++ shortestPathAlgorithm.cpp
@@ -1,19 +1,15 @@
 #include <stdio.h>
 #include <limits.h>
-
-
 #define V 21
 
 int minDistance(int dist[],
                 bool sptSet[])
 {
     int min = INT_MAX, min_index;
-
     for (int v = 0; v < V; v++)
         if (sptSet[v] == false &&
                    dist[v] <= min)
             min = dist[v], min_index = v;
-
     return min_index;
 }
 
@@ -21,9 +17,7 @@ void printPath(int parent[], int j)
 {
     if (parent[j] == - 1)
         return;
-
     printPath(parent, parent[j]);
-
     printf("%d ", j);
 }
 
@@ -44,36 +38,20 @@ int printSolution(int dist[], int n,
 void dijkstra(int graph[V][V], int src)
 {
     int dist[V];
-	
     bool sptSet[V];
-
-    
-    int parent[V];
-
-    
+    int parent[V]; 
     for (int i = 0; i < V; i++)
     {
         parent[0] = -1;
         dist[i] = INT_MAX;
         sptSet[i] = false;
     }
-
-    
     dist[src] = 0;
-
-    
     for (int count = 0; count < V - 1; count++)
     {
-        
         int u = minDistance(dist, sptSet);
-
-       
-        sptSet[u] = true;
-
-        
+        sptSet[u] = true;   
         for (int v = 0; v < V; v++)
-
-           
             if (!sptSet[v] && graph[u][v] &&
                 dist[u] + graph[u][v] < dist[v])
             {
@@ -81,8 +59,6 @@ void dijkstra(int graph[V][V], int src)
                 dist[v] = dist[u] + graph[u][v];
             }
     }
-
-   
     printSolution(dist, V, parent);
 }
 
